1c65d39f5ba42d9b7c83475ec6f8e06f152a8a00
Jed Segura <8326@stuller.com>
2026-05-15 14:36:39 -0500

    added some timers and stuff

--- Test_Conduit_Plugin/Test_CachedMesh_VS_Brep.cs
+++ Test_Conduit_Plugin/Test_CachedMesh_VS_Brep.cs
@@ -4,6 +4,7 @@ using Rhino.Display;
 using Rhino.Geometry;
 using Rhino.Input.Custom;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 
 namespace Test_Conduit_Plugin
@@ -11,7 +12,6 @@ namespace Test_Conduit_Plugin
     /* Purpose of this command is to try and figure out if there are any differences in caching a mesh for use in the display conduit vs just using the brep
      * Right now we are storing this mesh internally, but it SEEMS that it doesn't make anything more performant? So we're just adding bloat for no reason?
      * Or perhaps we are recalcing those meshes too often, thereby foregoing any performance gains we would have otherwise gotten?
-     * 
      */
 
     public class Test_CachedMesh_VS_Brep : Rhino.Commands.Command
@@ -22,6 +22,7 @@ namespace Test_Conduit_Plugin
 
         private static bool _useCachedMesh = false;
         private static double _sphereCount = 100;
+        private static double _sphereSize = 1.1;
 
         private readonly DrawConduit _drawConduit = new DrawConduit();
         private static DisplayMaterial _displayMat = new DisplayMaterial
@@ -46,48 +47,19 @@ namespace Test_Conduit_Plugin
 
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
-            _brepList.Clear();
-            _meshCache.Clear();
 
-            for (int i = 0; i < _sphereCount; i++)
-            {
-                //Create a bunch of spheres
-                var sphere1 = new Sphere(Point3d.Origin, 1.05);
-                sphere1.Translate(Vector3d.XAxis * i);
-                var brepSphere1 = sphere1.ToBrep();
-
-                //Make even more spheres
-                var sphere2 = sphere1;
-                sphere2.Translate(Vector3d.YAxis * i);
-                var brepSphere2 = sphere2.ToBrep();
-
-                //If we're in mesh mode we need our meshes to display
-                if (_useCachedMesh)
-                {
-                    var currMeshes = Mesh.CreateFromBrep(brepSphere1, MeshingParameters.FastRenderMesh);
-                    foreach (var currMesh in currMeshes)
-                    {
-                        _meshCache.Add(currMesh);
-                    }
-
-                    var currMeshes2 = Mesh.CreateFromBrep(brepSphere2, MeshingParameters.FastRenderMesh);
-                    foreach (var currMesh in currMeshes2)
-                    {
-                        _meshCache.Add(currMesh);
-                    }
-                }
-
-                _brepList.Add(brepSphere1);
-                _brepList.Add(brepSphere2);
-            }
+            CreateSpheres();
 
             _drawConduit.Enabled = true;
 
             GetOption go = new GetOption();
+            go.AcceptNothing(true);
+
             OptionToggle brepMeshToggle = new OptionToggle(_useCachedMesh, "Brep", "Mesh");
             go.AddOptionToggle("Use_Mesh", ref brepMeshToggle);
-            go.AcceptNothing(true);
-            go.SetCommandPrompt("Brep_or_Mesh");
+
+            OptionDouble sphereSizeOption = new OptionDouble(_sphereSize, 0.1, 3);
+            go.AddOptionDouble("Sphere_Size", ref sphereSizeOption);
 
             for(; ; )
             {
@@ -98,26 +70,64 @@ namespace Test_Conduit_Plugin
                 }
 
                 _useCachedMesh = brepMeshToggle.CurrentValue;
-
-                //If we've flipped to Mesh Mode and don't have any, lets go make them
-                if(_useCachedMesh && _meshCache.Count != _brepList.Count)
+                if(go.OptionIndex() == 1)
                 {
-                    _meshCache.Clear();
-                    for (int i = 0; i < _brepList.Count; i++)
-                    {
-                        var currMeshes = Mesh.CreateFromBrep(_brepList[i], MeshingParameters.FastRenderMesh);
-                        foreach (var currMesh in currMeshes)
-                        {
-                            _meshCache.Add(currMesh);
-                        }
-                    }
+                    _sphereSize = sphereSizeOption.CurrentValue;
                 }
+
+                //Update spheres with new options
+                CreateSpheres();
             }
 
             doc.Views.Redraw();
 
             // ---
             return Result.Success;
+        }
+
+        public void CreateSpheres()
+        {
+            var sphereGenTime = new Stopwatch();
+            sphereGenTime.Start();
+
+            _brepList.Clear();
+            _meshCache.Clear();
+
+            for (int i = 0; i < _sphereCount; i++)
+            {
+                //Create a bunch of spheres
+                var sphereX = new Sphere(Point3d.Origin, _sphereSize);
+                sphereX.Translate(Vector3d.XAxis * i);
+                var brepSphereX = sphereX.ToBrep();
+
+                //Even more
+                var sphereY = new Sphere(Point3d.Origin, _sphereSize);
+                sphereY.Translate(Vector3d.YAxis * i);
+                var brepSphereY = sphereY.ToBrep();
+
+                //If we're in mesh mode we need our meshes to display
+                if (_useCachedMesh)
+                {
+                    var currMeshes = Mesh.CreateFromBrep(brepSphereX, MeshingParameters.FastRenderMesh);
+                    foreach (var currMesh in currMeshes)
+                    {
+                        _meshCache.Add(currMesh);
+                    }
+
+                    var currMeshes2 = Mesh.CreateFromBrep(brepSphereY, MeshingParameters.FastRenderMesh);
+                    foreach (var currMesh in currMeshes2)
+                    {
+                        _meshCache.Add(currMesh);
+                    }
+                }
+
+                _brepList.Add(brepSphereX);
+                _brepList.Add(brepSphereY);
+            }
+
+            sphereGenTime.Stop();
+            Rhino.RhinoApp.WriteLine($"Sphere Gen Time: {sphereGenTime.ElapsedMilliseconds}ms");
+
         }
 
         //Here is the conduit where we draw our objects
@@ -133,6 +143,10 @@ namespace Test_Conduit_Plugin
 
             protected override void PostDrawObjects(DrawEventArgs e)
             {
+                //Write out how long it takes to draw our objects (really just need to look at init)
+                var sphereDrawTime = new Stopwatch();
+                sphereDrawTime.Start();
+
                 //Draw objects based on mode
                 if (_useCachedMesh)
                 {
@@ -149,6 +163,9 @@ namespace Test_Conduit_Plugin
 
                     }
                 }
+
+                sphereDrawTime.Stop();
+                Rhino.RhinoApp.WriteLine($"Draw Time: {sphereDrawTime.ElapsedMilliseconds}ms");
             }
         }
     }
